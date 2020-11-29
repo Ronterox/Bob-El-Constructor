bda348d81aeb124da3d36733b8e4d16484d71f59
Jonathan Vera <jonathan.vera.7e3@itb.cat>
2020-11-29 21:39:29 +0100

    BOB-15-part2-Death timer

---
+++ Assets/Scripts/HUD/GemCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GemCounter : MonoBehaviour {
+
+    public static GemCounter gemCounter;
+    int score = 0;
+    public TMP_Text scoretext;
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoretext = GameObject.Find("Gems Counter").GetComponent<TMP_Text>();
+        scoretext.text = score+"x";
+		
+	}
+
+    public void RaiseScore(int s)
+    {
+        score += s;
+        scoretext.text = score+"x";
+    }
+}
---
+++ Assets/Scripts/Interactables/Gem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GemCounter.gemCounter.RaiseScore(1);
+        Destroy(transform.parent.gameObject);
+    }
+}
