9a645941b72bf5dd85b0e407dc2483e14cabd5a9
Ricardo <ricarditomontserrat@gmail.com>
2020-11-30 21:32:08 +0100

    BOB-30: Fixed Death Timer and Added some prefabs, plus changed input

--- Assets/Scripts/GUI/Timer.cs
+++ Assets/Scripts/GUI/Timer.cs
@@ -2,30 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
 
-    public TMP_Text timerText;
-    public float timer = 0f;
-    private float pf_mins;
-    private bool pb_isStop;
-
-
-
+    [SerializeField] private float timerTime;
+    private float pf_timer = 0f;
     void Update()
     {
-        if (pb_isStop)
-        {
-            return;
-        }
-        timer -= Time.deltaTime;
-        if (timer < 0)
-        {
-            timer = 0;
-            pb_isStop = true;
-        }
-        timerText.text = string.Format("{0:00}:{1:00}", timer / 60 % 60, timer%60);
-
+        if (pf_timer < 0) return;
+        pf_timer -= Time.deltaTime;
+        if (pf_timer < 0) pf_timer = 0;
+        timerText.text = string.Format("{0:00}:{1:00}", pf_timer / 60 % 60, pf_timer % 60);
     }
 
-   
+    public void StartTimer()
+    {
+        pf_timer = timerTime;
+    }
 }
