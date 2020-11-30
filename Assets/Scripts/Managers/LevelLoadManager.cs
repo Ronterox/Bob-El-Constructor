deb8c96a40ea73014c67182d95d620c504411112
Ricardo <ricarditomontserrat@gmail.com>
2020-12-01 00:37:27 +0100

    BOB-38: Updated Readme and added basic LevelLoadManager

---
+++ Assets/Scripts/Managers/LevelLoadManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoadManager : MonoBehaviour 
+{
+    [SerializeField] private Scene[] additiveScenes;
+
+    //Add on SceneLoad Event
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+    }
+    public void loadAdditiveScenes()
+    {
+        //Check if is load
+        foreach(Scene scene in additiveScenes)
+        {
+            SceneManager.LoadSceneAsync(scene.buildIndex, LoadSceneMode.Additive);
+        }
+    }
+}
