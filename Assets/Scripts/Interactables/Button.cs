deadf375c563854fcac6a48d6bc8c6823edc3dc4
Jonathan Vera <jonathan.vera.7e3@itb.cat>
2020-12-03 01:06:36 +0100

    BOB-57: Pressure plate that open a door when trigger a object

---
+++ Assets/Scripts/Interactables/Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[System.Serializable]
+
+public class OnButtonEvent:UnityEvent
+{
+
+}
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private OnButtonEvent onButtonEventEnter;
+    [SerializeField] private OnButtonEvent onButtonEventExit;
+    private int p_CollisionCounter;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        p_CollisionCounter++;
+        if (p_CollisionCounter > 0) { onButtonEventEnter.Invoke(); }
+       
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        p_CollisionCounter--;
+        if (p_CollisionCounter == 0) { onButtonEventExit.Invoke(); }
+       
+    }
+
+
+}
