6f9f5e7e04193aaf1ba56d808372e2451bf7c3d5
Ricardo <ricarditomontserrat@gmail.com>
2020-12-03 22:26:39 +0100

    BOB-62,58,46: Fixed Merge on Develop

--- Assets/Scripts/Interactables/Button.cs
+++ Assets/Scripts/Interactables/Button.cs
@@ -2,32 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+
 [System.Serializable]
-
-public class OnButtonEvent:UnityEvent
-{
-
-}
+public class OnButtonEvent:UnityEvent {}
 
 public class Button : MonoBehaviour
 {
     [SerializeField] private OnButtonEvent onButtonEventEnter;
     [SerializeField] private OnButtonEvent onButtonEventExit;
     private int p_CollisionCounter;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         p_CollisionCounter++;
-        if (p_CollisionCounter > 0) { onButtonEventEnter.Invoke(); }
-       
-       
+        onButtonEventEnter.Invoke();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         p_CollisionCounter--;
         if (p_CollisionCounter == 0) { onButtonEventExit.Invoke(); }
-       
     }
-
-
 }
---
+++ Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour 
+{
+    private Animator p_animator;
+    private void Awake() { p_animator = GetComponent<Animator>(); }
+    public void OpenDoor() { p_animator.SetBool("open", true); }
+    public void CloseDoor() { p_animator.SetBool("open", false); }
+}
