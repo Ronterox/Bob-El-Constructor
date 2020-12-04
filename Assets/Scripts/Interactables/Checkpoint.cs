df590be5d81a7b2f6764893077c48792772c7fcf
Jonathan Vera <jonathan.vera.7e3@itb.cat>
2020-12-04 17:56:45 +0100

    BOB-54 Creation of first checkpoint(need to improve)

---
+++ Assets/Scripts/Interactables/Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class onCollision : UnityEvent { }
+
+public class Checkpoint : MonoBehaviour {
+
+    [SerializeField] private onCollision enterCheckpoint;
+    public static Vector3 reachedPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (transform.position.x > reachedPoint.x)
+            {
+                reachedPoint = transform.position;
+                enterCheckpoint.Invoke();
+            }
+         
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Respawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+    private bool pb_isRestarting = false;
+    [SerializeField]private int pi_fallDistance;
+
+
+    private void Update()
+    {
+        if (transform.position.y <= pi_fallDistance)
+        {
+            if (pb_isRestarting == false)
+            {
+                respawnPlayer();
+            }
+        }
+     
+    }
+
+  
+     public void respawnPlayer()
+    {
+        pb_isRestarting = true;
+        transform.position = Checkpoint.reachedPoint;
+        pb_isRestarting = false;
+
+
+    }
+}
