a085cf52f675ee9ffeca17c52b2fbeaabea86b3b
Ricardo <ricarditomontserrat@gmail.com>
2020-12-05 20:40:19 +0100

    BOB-54: Added checkpoint variable to GameManager and deleted Trial Script Respawn

--- Assets/Scripts/Interactables/Checkpoint.cs
+++ Assets/Scripts/Interactables/Checkpoint.cs
@@ -6,21 +6,17 @@ using UnityEngine.Events;
 [System.Serializable]
 public class onCollision : UnityEvent { }
 
-public class Checkpoint : MonoBehaviour {
-
+public class Checkpoint : MonoBehaviour
+{
     [SerializeField] private onCollision enterCheckpoint;
-    public static Vector3 reachedPoint;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
-            if (transform.position.x > reachedPoint.x)
-            {
-                reachedPoint = transform.position;
-                enterCheckpoint.Invoke();
-            }
-         
+            GameManager.instance.checkpoint = transform;
+            gameObject.SetActive(false);
+            enterCheckpoint.Invoke();
         }
     }
 }
