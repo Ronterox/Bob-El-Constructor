a69eb21a02cae2fcbfce2e1181da55c6e34215c1
Jonathan Vera <jonathan.vera.7e3@itb.cat>
2020-12-05 21:23:10 +0100

    BOB-65 Move object(Incomplete)

---
+++ Assets/Scripts/Interactables/MoveObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+    [SerializeField] private Vector2 xy;
+    [SerializeField] private float velocity;
+    private bool p_isPressed;
+    private Rigidbody2D p_rb;
+
+    private void Awake()
+    {
+        p_rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (p_isPressed)
+        {
+            if (gameObject.transform.position.x != xy.x && gameObject.transform.position.y != xy.y)
+            {
+                p_rb.MovePosition(xy);
+            }
+            else
+            {
+                p_isPressed = false;
+            }
+          
+           
+        }
+    }
+
+    public void Teleport()
+    {
+        gameObject.transform.position = xy;
+    }
+
+    public void Move()
+    {
+        p_isPressed = true; 
+    }
+}
