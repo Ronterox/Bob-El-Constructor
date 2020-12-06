0169e925c59f3963e0ef304aa233b5dc2da3c3ad
Ricardo <ricarditomontserrat@gmail.com>
2020-12-06 22:02:59 +0100

    BOB-72: Implemented area 4 for level 1, and created Basic Spawner

---
+++ Assets/Scripts/Tools/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour 
+{
+    //TODO: Have them as childs to detect how many are on the scene right now.
+
+    [SerializeField] private GameObject toSpawnObject;
+
+    [Header("Limit Options")]
+    [SerializeField] private int spawnLimit;
+    [SerializeField] private bool limitSpawns;
+
+    [Header("For Other Methods")]
+    /*
+    [SerializeField] readonly int spawnedObjects
+    {
+        get
+        {
+            return totalObjectsSpawned;
+        }
+    }*/
+
+    private int totalObjectsSpawned;
+
+    /// <summary>
+    /// Spawns the object
+    /// </summary>
+    public void SpawnObject()
+    {
+        if (limitSpawns && totalObjectsSpawned >= spawnLimit) return;
+        Instantiate(gameObject, transform.position, Quaternion.identity);
+        totalObjectsSpawned++;
+    }
+
+    /// <summary>
+    /// Deletes all the instantiated objects
+    /// </summary>
+    public void DeleteAllSpawnedObjects()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SpawnObjectAt()
+    {
+
+    }
+}
