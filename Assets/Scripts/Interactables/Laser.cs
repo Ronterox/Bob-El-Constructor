590fa6e1dc3a4b20084c49690a0a2885931b97aa
Lelouny <jonathan.vera.7e3@itb.cat>
2021-01-05 23:45:47 +0100

    BOB-60 Improve lvl1 and added a gravity gun(need to improve))

---
+++ Assets/Scripts/Interactables/Laser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+
+{
+    public Camera cam;
+    public LineRenderer line;
+    public Transform firepoint;
+    private Quaternion p_rotation;
+
+    void Start()
+    {
+        DisableLaser();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            EnableLaser();
+
+        }
+        if(Input.GetButton("Fire2"))
+        {
+            UpdateLaser();
+        }
+        if(Input.GetButtonUp("Fire2"))
+        {
+            DisableLaser();
+        }
+
+        RotateToMouse();
+   
+    }
+
+    void DisableLaser()
+    {
+        line.enabled = false;
+    }
+
+     void UpdateLaser()
+    {
+        var mousepos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        line.SetPosition(0, firepoint.position);
+        line.SetPosition(1, mousepos);
+    }
+
+    void EnableLaser()
+    {
+        line.enabled = true;
+    }
+
+    void RotateToMouse()
+    {
+        Vector2 direction= cam.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+        p_rotation.eulerAngles = new Vector3(0, 0, angle);
+        transform.rotation = p_rotation;
+    }
+}
