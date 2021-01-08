707a6f0654da6f24b9e69c47275195e3d42d7d3b
Ricardo <ricarditomontserrat@gmail.com>
2021-01-09 00:16:28 +0100

    Added Temp Assets to demo cutscene on demo-lvl3

---
+++ Assets/Scripts/Managers/PlaySfxOnStart.cs
@@ -0,0 +1,12 @@
+﻿using Plugins.Tools;
+using UnityEngine;
+
+namespace Managers
+{
+    public class PlaySfxOnStart : MonoBehaviour
+    {
+        [SerializeField] private string sound;
+
+        private void Start() => SoundManager.Instance.Play(sound);
+    }
+}
