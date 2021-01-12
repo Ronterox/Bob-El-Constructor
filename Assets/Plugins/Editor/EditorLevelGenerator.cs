fb5766b83c550e800482c7cdeb1367ecf6b1c904
Ricardo <ricarditomontserrat@gmail.com>
2021-01-12 23:15:43 +0100

    BOB-122: Reorganized scripts using UnityEditor

---
+++ Assets/Plugins/Editor/EditorLevelGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Plugins.Editor
+{
+    [CustomEditor(typeof(LevelGenerator))]
+    public class EditorLevelGenerator : UnityEditor.Editor
+    {
+        private LevelGenerator p_script;
+
+        private void OnEnable() => p_script = target as LevelGenerator;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUILayout.Space(5);
+            if (GUILayout.Button("Generate Level")) p_script.GenerateLevel();
+        }
+    }
+}
