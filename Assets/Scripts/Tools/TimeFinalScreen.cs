20697a9c9dac720da9ed95ba54da4279b1caa2be
Ricardo <ricarditomontserrat@gmail.com>
2021-01-24 23:11:29 +0100

    BOB-141: Added Final Cutscene

---
+++ Assets/Scripts/Tools/TimeFinalScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using Managers;
+using Plugins.Tools;
+using UnityEngine;
+
+public class TimeFinalScreen : MonoBehaviour
+{
+    [SerializeField] private float timeToWait = 1f;
+
+    public void StartTimeCall() => StartCoroutine(TimeCall());
+
+    private IEnumerator TimeCall()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LevelLoadManager.Instance.LoadNextScene();
+    }
+
+    public void JumpScare() => SoundManager.Instance.Play("JUMPSCARE");
+}
